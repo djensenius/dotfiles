1cc8f89e70547ccd5cc5019bcfccecb7ca5098fd
David Jensenius <david@jensenius.com>
2022-04-19 21:47:04 -0400

    Add clippy config, more tweaks

--- fish/config.fish
+++ fish/config.fish
@@ -5,10 +5,11 @@
 # set -g fish_user_paths "$HOME/.cargo/bin" $fish_user_paths
 fish_add_path /home/linuxbrew/.linuxbrew/bin
 fish_add_path /home/linuxbrew/.linuxbrew/sbin:
-if test -e /workspaces/.codespaces/shared/.env
-end
 starship init fish | source
 # status --is-interactive; and source (rbenv init -|psub)
 # set -g fish_user_paths "/usr/local/opt/openssl@1.1/bin" $fish_user_paths
 # set -g fish_user_paths "/usr/local/opt/ncurses/bin" $fish_user_paths
 status --is-interactive; and rbenv init - fish | source
+if test -e /workspaces/.codespaces/shared/.env
+  posix-source /workspaces/.condespaces/shared/.env
+end
