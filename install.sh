2594a27626373a6ae9d72ef39393b831ffadde32
David Jensenius <david@jensenius.com>
2022-07-22 11:00:00 -0400

    Remove sleep

--- install.sh
+++ install.sh
@@ -16,7 +16,6 @@ function install_software() {
     sudo mv nvim.appimage /usr/bin/nvim
     sudo chmod a+x /usr/bin/nvim
     curl -sS https://starship.rs/install.sh | sudo sh -s -- -y
-    sleep 5
     sudo apt -o DPkg::Lock::Timeout=600 install build-essential python3-venv kitty-terminfo socat ncat npm ruby-dev bat exa jq ripgrep thefuck tmux libfuse2 fuse fish git-extras software-properties-common -y
     sudo npm install -g typescript-language-server typescript vscode-langservers-extracted eslint_d
 }
