6d27d724c10a26afdd62988486f2b958934bfa5f
David Jensenius <david@jensenius.com>
2022-10-04 16:08:00 +0200

    Fix deprication

--- nvim/after/plugin/nvim-lspconfig.lua
+++ nvim/after/plugin/nvim-lspconfig.lua
@@ -102,9 +102,9 @@ local on_attach = function(client, bufnr)
     buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
 
     -- Set some keybinds conditional on server capabilities
-    if client.resolved_capabilities.document_formatting then
+    if client.server_capabilities.document_formatting then
         buf_set_keymap("n", "<space>=", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-    elseif client.resolved_capabilities.document_range_formatting then
+    elseif client.server_capabilities.document_range_formatting then
         buf_set_keymap("n", "<space>=", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
     end
 end
