f071635265774f23c4f81ba79fb52b29185a2716
David Jensenius <david@jensenius.com>
2023-01-25 10:21:00 -0500

    Eval brew with fish

--- fish/config.fish
+++ fish/config.fish
@@ -3,14 +3,15 @@
 # source $HOME/.cargo/env
 # set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
 # set -g fish_user_paths "$HOME/.cargo/bin" $fish_user_paths
-fish_add_path /home/linuxbrew/.linuxbrew/bin
-fish_add_path /home/linuxbrew/.linuxbrew/sbin
+# fish_add_path /home/linuxbrew/.linuxbrew/bin
+# fish_add_path /home/linuxbrew/.linuxbrew/sbin
 # fish_add_path /workspaces/github/bin
 starship init fish | source
 # status --is-interactive; and source (rbenv init -|psub)
 # set -g fish_user_paths "/usr/local/opt/openssl@1.1/bin" $fish_user_paths
 # set -g fish_user_paths "/usr/local/opt/ncurses/bin" $fish_user_paths
 thefuck --alias | source
+eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 if test -e /workspaces/.codespaces/shared/.env
   # posix-source /workspaces/.condespaces/shared/.env
 else
