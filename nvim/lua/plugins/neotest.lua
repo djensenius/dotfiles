d53e469f52575b5929a129039d3ea3585a1b7f3e
David Jensenius <david@jensenius.com>
2023-04-02 18:28:54 -0400

    Add neotest

---
+++ nvim/lua/plugins/neotest.lua
@@ -0,0 +1,28 @@
+return {
+  'nvim-neotest/neotest',
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-treesitter/nvim-treesitter',
+    'antoinemadec/FixCursorHold.nvim',
+    'haydenmeade/neotest-jest'
+  },
+  config = function()
+    require("neotest").setup({
+      adapters = {
+        require('neotest-jest')({
+          jestCommand = "npx jest --watch",
+        }),
+      }
+    })
+    vim.cmd [[
+      command! NeotestSummary lua require("neotest").summary.toggle()
+      command! NeotestFile lua require("neotest").run.run(vim.fn.expand("%"))
+      command! Neotest lua require("neotest").run.run(vim.fn.getcwd())
+      command! NeotestNearest lua require("neotest").run.run()
+      command! NeotestDebug lua require("neotest").run.run({ strategy = "dap" })
+      command! NeotestAttach lua require("neotest").run.attach()
+      command! NeotestOutput lua require("neotest").output.open()
+    ]]
+  end,
+}
