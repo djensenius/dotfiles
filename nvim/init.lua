068f2992e25475a883deb13a21d811bbb97dfeb7
David Jensenius <david@jensenius.com>
2023-05-25 09:59:19 -0400

    Move some basic stuff around

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,5 @@
 vim.opt.termguicolors = true
+require("basic")
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -14,6 +15,4 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader=","
 require("lazy").setup("plugins")
 vim.opt.shell = '/bin/zsh'
-require("basic")
 require("keys")
-
