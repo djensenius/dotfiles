21475baf8ab31050cc1f342ef9b965fe17028c03
David Jensenius <david@jensenius.com>
2023-05-31 15:05:26 -0400

    Lint

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -49,7 +49,6 @@ return {
         ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
         ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
         ["<m-o>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-        -- ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
         ["<C-c>"] = cmp.mapping {
           i = cmp.mapping.abort(),
           c = cmp.mapping.close(),
