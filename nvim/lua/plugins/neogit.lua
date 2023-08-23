51cbb8ca2f8eeda44d00070f43a60525191f026d
David Jensenius <david@jensenius.com>
2023-08-23 14:38:20 -0400

    Add neogit

---
+++ nvim/lua/plugins/neogit.lua
@@ -0,0 +1,10 @@
+return {
+  "NeogitOrg/neogit",
+  event = "VeryLazy",
+  dependencies = {
+    "nvim-lua/plenary.nvim",         -- required
+    "nvim-telescope/telescope.nvim", -- optional
+    "sindrets/diffview.nvim",        -- optional
+  },
+  config = true
+}
