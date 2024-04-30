b19bb285c8a6ff1b01f3839c6b24387b919abbd2
David Jensenius <david@jensenius.com>
2024-04-29 22:48:20 -0400

    Oops, forgot to lazy load nav

--- nvim/lua/plugins/vim-tmux-navigator.lua
+++ nvim/lua/plugins/vim-tmux-navigator.lua
@@ -1,5 +1,6 @@
 return {
 	"christoomey/vim-tmux-navigator",
+	event = "VeryLazy",
 	vim.keymap.set("n", "C-h", ":TmuxNavigateLeft<CR>"),
 	vim.keymap.set("n", "C-j", ":TmuxNavigateDown<CR>"),
 	vim.keymap.set("n", "C-k", ":TmuxNavigateUp<CR>"),
