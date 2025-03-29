33a2cb9a3de837e71f810107f388c789161d95f9
David Jensenius <david@jensenius.com>
2025-03-29 10:34:07 -0400

    Tweak LSP icons and virtual lines

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -32,19 +32,30 @@ return {
 
       -- Virtual line stuff
       vim.diagnostic.config({
-        virtual_lines = true,
+        virtual_lines = { current_line = true },
         underline = { severity = vim.diagnostic.severity.WARN }, -- underlines for warnings and errors only
         virtual_text = {
-          prefix = '●',
+          prefix = function(diagnostic)
+            if diagnostic.severity == vim.diagnostic.severity.ERROR then
+              return ""
+            elseif diagnostic.severity == vim.diagnostic.severity.WARN then
+              return ""
+            elseif diagnostic.severity == vim.diagnostic.severity.INFO then
+              return ""
+            else
+              return ""
+            end
+
+          end,
           source = "if_many",
           spacing = 4,
         },
         signs = {
           text = {
-            [vim.diagnostic.severity.ERROR] = "",
-            [vim.diagnostic.severity.WARN] = "",
-            [vim.diagnostic.severity.INFO] = "󰋼",
-            [vim.diagnostic.severity.HINT] = "󰌵",
+            [vim.diagnostic.severity.ERROR] = "",
+            [vim.diagnostic.severity.WARN] = "",
+            [vim.diagnostic.severity.INFO] = "",
+            [vim.diagnostic.severity.HINT] = "",
           },
         }
       })
