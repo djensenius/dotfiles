f7a5ca3f35253de2d8af5502b70d271c120e6da8
David Jensenius <david@jensenius.com>
2025-08-06 13:30:06 -0400

    Fix codespace paths, notify error

--- fish/conf.d/60-workspaces.fish
+++ fish/conf.d/60-workspaces.fish
@@ -1,18 +1,42 @@
-# Codespaces/Devcontainer adjustments
+# Codespaces/workspaces-specific PATH fixes
+
 if test -d /workspaces
-    # npm global prefix PATH (fixed to fish command substitution)
-    set -l npm_prefix (npm config get prefix 2>/dev/null)
-    if test -n "$npm_prefix"
-        fish_add_path -g $npm_prefix $npm_prefix/bin
+    # 1) Vendored Node bins (e.g., /workspaces/github/vendor/node/node-vXX.../bin)
+    for dir in /workspaces/*/vendor/node/*/bin
+        if test -d $dir
+            fish_add_path -g $dir
+        end
     end
 
-    # fzf binaries in Codespaces
-    if test -d ~/.fzf/bin
-        fish_add_path -g ~/.fzf/bin
+    # 2) npm global bin (only if npm exists)
+    if command -q npm
+        set -l npm_prefix (npm config get prefix 2>/dev/null)
+        if test -n "$npm_prefix"; and test -d "$npm_prefix/bin"
+            fish_add_path -g "$npm_prefix/bin"
+        end
     end
-end
 
-# Custom container marker for Neovim path
-if test -e /etc/dfj-container
-    fish_add_path -g /opt/nvim-linux64/bin
+    # 3) Ruby: expose rbenv "bin" locations without enabling shims
+    for r in /root/.rbenv/bin /usr/local/share/rbenv/bin
+        if test -d $r
+            fish_add_path -g $r
+        end
+    end
+
+    # 4) Go toolchains
+    for g in /usr/local/go/bin /go/bin
+        if test -d $g
+            fish_add_path -g $g
+        end
+    end
+
+    # 5) per-user fzf bin
+    if test -d "$HOME/.fzf/bin"
+        fish_add_path -g "$HOME/.fzf/bin"
+    end
+
+    # 6) Match parent PATH oddity if present
+    if test -d /usr/local/share
+        fish_add_path -g /usr/local/share
+    end
 end
--- nvim/lua/plugins/nvim-notify.lua
+++ nvim/lua/plugins/nvim-notify.lua
@@ -4,7 +4,6 @@ return {
 	config = function()
 		require("notify").setup({
 			stages = "fade_in_slide_out",
-			background_colour = "FloatShadow",
 			timeout = 2000,
 		})
 		vim.notify = require("notify")
