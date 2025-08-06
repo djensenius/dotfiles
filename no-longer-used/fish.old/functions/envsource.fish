d33e6e3ae67a304f4ac5bce71c2ae2dd2e2fa063
David Jensenius <david@jensenius.com>
2025-08-06 11:06:50 -0400

    Keep around old fish config

---
+++ no-longer-used/fish.old/functions/envsource.fish
@@ -0,0 +1,11 @@
+# Place this in your Fish functions folder to make it available immediately
+# e.g. ~/.config/fish/functions/envsource.fish
+#
+# Usage: envsource <path/to/env>
+
+function envsource
+  for line in (cat $argv | grep -v '^#')
+    set item (string split -m 1 '=' $line)
+    set -gx $item[1] $item[2]
+  end
+end
