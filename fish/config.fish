62d908e03bf546ba78dc8c48460f811cb69c1ed2
David Jensenius <david@jensenius.com>
2025-10-30 11:48:39 +0100

    Fix indentation

--- fish/config.fish
+++ fish/config.fish
@@ -5,7 +5,7 @@ if status is-interactive
     if test -d /workspaces
         __fish_reconstruct_path
     end
-    
+
     # Prompt and tools that hook into the interactive shell
     starship init fish | source
     zoxide init fish | source
@@ -22,7 +22,7 @@ if status is-interactive
 
     # Optional extra plugin
     command -q pay-respects; and pay-respects fish | source
-    
+
     # Initialize mise for interactive shells
     if command -q mise
         mise activate fish | source
