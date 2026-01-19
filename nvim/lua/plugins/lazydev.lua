16cc5361e102a46be7275ed76eb0447d9629ef79
David Jensenius <david@jensenius.com>
2026-01-19 00:17:02 -0500

    Some lazydev path info

--- nvim/lua/plugins/lazydev.lua
+++ nvim/lua/plugins/lazydev.lua
@@ -7,6 +7,8 @@ return {
 				-- See the configuration section for more details
 				-- Load luvit types when the `vim.uv` word is found
 				{ path = "luvit-meta/library", words = { "vim%.uv" } },
+				{ path = "snacks.nvim", words = { "Snacks" } },
+				{ path = "lazy.nvim", words = { "LazyVim" } },
 			},
 		},
 	},
